e3821326e6f696c9402a5a385ba30e7493e89e8b
MAshry007 <ashrym99@gmail.com>
2026-09-08 20:25:33 +0300

    Starting Notes App

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: NotesView.id,
+      routes: {NotesView.id: (context) => NotesView()},
+      theme: ThemeData.dark(),
+    );
+  }
+}
