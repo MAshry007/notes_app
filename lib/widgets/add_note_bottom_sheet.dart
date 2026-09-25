7514080585565a9d5f7a89acd54775e847cda2cf
MAshry007 <ashrym99@gmail.com>
2026-09-25 20:06:31 +0300

    Create/ add note form Validate/ custom button Add/ Gesture detector on custom button Validate/ custom text field

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_button.dart';
-import 'package:notes_app/widgets/custom_text_field.dart';
+import 'package:notes_app/widgets/add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
@@ -9,19 +8,7 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 32),
-            CustomTextField(hint: 'Title'),
-            SizedBox(height: 16),
-            CustomTextField(hint: 'Content', maxLines: 5),
-            SizedBox(height: 130),
-            CustomButton(),
-            SizedBox(height: 60),
-          ],
-        ),
-      ),
+      child: SingleChildScrollView(child: AddNoteForm()),
     );
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -3,23 +3,27 @@ import 'package:notes_app/widgets/constants.dart';
 import 'package:notes_app/widgets/custom_text.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, this.onTap});
 
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: kPrimaryColor,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      height: 55,
-      width: MediaQuery.of(context).size.width,
-      child: Center(
-        child: CustomText(
-          text: 'Add',
-          color: Colors.black,
-          size: 20,
-          weight: FontWeight.bold,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        height: 55,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: CustomText(
+            text: 'Add',
+            color: Colors.black,
+            size: 20,
+            weight: FontWeight.bold,
+          ),
         ),
       ),
     );
